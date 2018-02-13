c8595513d68112e4d6ef9f46c112a248d4e4c24e
MEOW <mdigitalcomm@gmail.com>
2018-02-13 00:05:10 -0500

    created functions
    Only flipCard works, the others don't function well yet, need more work on them.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 /*
  * Create a list that holds all of your cards
  */
+let array = [...document.querySelectorAll('.card')];
+
+// Display the cards on the page
+//   - shuffle the list of cards using the provided "shuffle" method below
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+//   - loop through each card and create its HTML
+
+
+//   - add each card's HTML to the page
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -29,9 +32,48 @@ function shuffle(array) {
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+*/
+
+let deck = document.querySelector('.deck');
+let open = document.querySelectorAll('.open');
+let move = 0;
+
+function flipCard(evt) {
+	evt.target.classList.toggle('open');
+	evt.target.classList.toggle('show');
+}
+
+function lock() {	
+ 	open.classList.add('match');
+ 	open.classList.remove('show');
+ 	open.classList.remove('open');	
+}
+
+function unflip() {
+	open.classList.remove('show');
+ 	open.classList.remove('open');
+}	
+
+
+function matchCard() {
+	if (open.length === 2 && open[0].isEqualNode(open[1])) {
+		lock();
+	} else if (open.length === 2 && !open[0].isEqualNode(open[1])) {
+		unflip();
+	}
+	return move;	
+}
+
+deck.addEventListener('click', flipCard);
+move++;
+matchCard();
+ 
+ //  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ 
+//  - if the list already has another card, check to see if the two cards match
+ 
+
+ /*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
