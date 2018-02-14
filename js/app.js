973e84be55b3581e5acab0eabbcc2d42eacb0b7a
MEOW <mdigitalcomm@gmail.com>
2018-02-13 21:12:08 -0500

    Fixed flipCard, matchCard function
    Still need to fix congrat(), and count moves

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-let array = [...document.querySelectorAll('.card')];
+let array = [...document.getElementsByClassName('card')];
 
 // Display the cards on the page
 //   - shuffle the list of cards using the provided "shuffle" method below
@@ -32,49 +32,69 @@ function shuffle(array) {
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
+ * - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
 */
 
+
 let deck = document.querySelector('.deck');
-let open = document.querySelectorAll('.open');
+let open = document.getElementsByClassName('open');
+let match = document.getElementsByClassName('match');
 let move = 0;
 
 function flipCard(evt) {
 	evt.target.classList.toggle('open');
 	evt.target.classList.toggle('show');
+
 }
 
-function lock() {	
- 	open.classList.add('match');
- 	open.classList.remove('show');
- 	open.classList.remove('open');	
+
+/*  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+*/ 
+
+function matchCard() {
+	if (open.length === 2) {
+		if (open[0].innerHTML === open[1].innerHTML) {lock();}
+		else {unflip();}	
+	}
+	setTimeout(matchCard, 2000);
+}
+
+function lock() {	 	
+	for (i=0; i<2; i++) {
+		open[i].classList.add('match');
+		open[i].classList.remove('show');
+	}	
+	open[0].classList.remove('open');
+	open[0].classList.remove('open');
 }
 
 function unflip() {
-	open.classList.remove('show');
- 	open.classList.remove('open');
-}	
+	for (let i=0; i<2; i++) {
+		open[i].classList.remove('show');	
+ 	}
+ 	open[0].classList.remove('open');
+ 	open[0].classList.remove('open');
 
-
-function matchCard() {
-	if (open.length === 2 && open[0].isEqualNode(open[1])) {
-		lock();
-	} else if (open.length === 2 && !open[0].isEqualNode(open[1])) {
-		unflip();
-	}
-	return move;	
 }
 
-deck.addEventListener('click', flipCard);
-move++;
-matchCard();
- 
- //  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- 
-//  - if the list already has another card, check to see if the two cards match
- 
+function congrat() {
+	if (match.length === 16) {
+	alert(`Congratulations!You've used x steps!`)
+	}
+}
 
- /*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+
+deck.addEventListener('click', function () {
+	deck.addEventListener('click', flipCard);
+	matchCard();
+});
+
+deck.addEventListener('click', congrat);
+
+
+ 
+/*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
