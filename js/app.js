f8dc3cde26ed790aabc9498c2bc8f459ecb10d28
MEOW <mdigitalcomm@gmail.com>
2018-02-15 13:53:45 -0500

    Added star rating function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
-let cards = Array.prototype.slice.call(document.getElementsByClassName('card'));
+let card = document.getElementsByClassName('card');
+let cards = Array.prototype.slice.call(card);
 const container = document.querySelector('.container')
 const restart = document.querySelector('.restart');
 let deck = document.getElementsByClassName('deck');
@@ -11,10 +12,11 @@ let moveN = 0;
 
 //   - shuffle the list of cards using the provided "shuffle" method below
 function reset() {
-	shuffle(cards);
-	unflipAll();
-	createNewCards();
 	moveN = 0;
+	stars();
+	unflipAll();
+	shuffle(cards);
+	createNewCards();	
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -35,8 +37,8 @@ function shuffle(array) {
 
 //Unflip all open cards before reset
 function unflipAll() {
-	while (match.length>0) {
-		match[0].classList.remove('match', 'shake', 'bump');
+	for (let i=0; i<16; i++) {
+		card[i].classList.remove('match', 'open', 'show', 'shake', 'bump');
 	}
 }
 
@@ -63,17 +65,21 @@ function flipCard(evt) {
 // If 2 cards are open, check to see if the two cards match
 function matchCard() {
 	if (open.length >= 2) {
+
 		//if the cards do match, lock the cards in the open position
 		if (open[0].innerHTML === open[1].innerHTML) {
 			animate('bump');
-			setTimeout(lock, 300);
+			setTimeout(lock, 500);
+			
 		}
 		
 		//  if the cards do not match, remove the cards from the list and hide the card's symbol
 		else {
 			animate('shake');
-			setTimeout(unflip, 300);
-		}		
+			setTimeout(unflip, 500);
+			
+		}
+		moveN++;
 	} 
 }
 
@@ -109,23 +115,49 @@ function printMove() {
 
 }
 
+//Set stars according to number of moves
+let starN = document.querySelector('.stars').children;
+function stars () {
+	const fullStar = "<i class= 'fa fa-star'>";
+	const halfStar = "<i class= 'fa fa-star-half-o'>";
+	const emStar = "<i class='fa fa-star-o'>";
+	if (moveN >= 0) {
+		for (let i=0; i<3; i++) {
+			starN[i].innerHTML = fullStar
+		}
+		if (moveN > 10) {
+			// 2.5 stars
+			starN[2].innerHTML = halfStar;
+			if (moveN > 13) {
+				starN[2].innerHTML = emStar;
+				if (moveN > 16) {
+					starN[1].innerHTML = halfStar;
+					if (moveN > 20) {
+						starN[1].innerHTML = emStar;
+					}
+				}
+			}
+		}
+	}	
+}
 
 // If all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 function congrat() {
 	if (match.length === 16) {
-	alert(`Congratulations! You used ${moveN} steps to finish!`)
+		if(confirm(`Congratulations! You used ${moveN} steps to finish! Would you like to have another one?`)) {
+			reset();
+		} 
 	}
 }
 
 
-container.addEventListener('click', function (evt) {	
-	
+container.addEventListener('click', function (evt) {		
 	if (evt.target.tagName === 'LI') {
 		setTimeout(flipCard(evt), 0);
-		moveN++;
 	}	
-	setTimeout(printMove, 0);
 	setTimeout(matchCard, 0);
+	setTimeout(printMove, 500);
+	setTimeout(stars, 0);
 	setTimeout(congrat, 3000);
 });
 
